06ba5fc7db09221a1504795d73aa993a27c755af
lulo <lopez.gallego2611@gmail.com>
2025-05-27 02:23:34 -0500

    cha cha cha changes

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,11 +14,8 @@ class User extends Authenticatable
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
+    protected $table = 'users';
+
     protected $fillable = [
         'name',
         'email',
