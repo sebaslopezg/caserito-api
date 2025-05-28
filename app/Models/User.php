17be0436ea91f4a2311428a071dbeb548c212c3b
lulo <lopez.gallego2611@gmail.com>
2025-05-28 18:35:36 -0500

    smth

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,14 +7,15 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Model;
 
-
-class User extends Authenticatable
+class User extends Model
 {
+    protected $table = 'users';
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasApiTokens, HasFactory, Notifiable;
 
-    protected $table = 'users';
+    
 
     protected $fillable = [
         'name',
